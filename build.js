767963e6501df35788f89b9951421cb76882490b
dommilosz <dommilosz@gmail.com>
2020-05-06 21:04:46 +0200

    upload

--- build.js
+++ build.js
@@ -18,8 +18,8 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'mlauncher',
+        productName: 'MLauncher',
         artifactName: '${productName}.${ext}',
         copyright: 'Copyright © 2018-2020 Daniel Scalzi',
         directories: {
